698c81b2ebabce434411c821c19dfa2912f0d567
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-11-30 16:28:26 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
         <p>
-         Welcome to Descope&apos;s NextJS Sample App
+         Welcome to Descope&apos;s NextJS Sample App!
         </p>
     </main>
   )
