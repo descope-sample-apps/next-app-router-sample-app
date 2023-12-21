4c241c3c4caba69b343c294f0592e2132d683ded
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-12-21 13:49:17 -0800

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,6 +8,7 @@ export default function Page() {
 
     const { isAuthenticated, isSessionLoading } = useSession();
 	const { isUserLoading } = useUser();
+    
     // Redirect to login if not authenticated
     useEffect(() => {
         if (!(isSessionLoading || isUserLoading) && isAuthenticated) {
