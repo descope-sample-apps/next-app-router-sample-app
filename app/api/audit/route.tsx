facb377f696e2e3075bae543f61d81925d30d8c8
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2024-01-18 12:53:03 -0800

    Update route.tsx

--- app/api/audit/route.tsx
+++ app/api/audit/route.tsx
@@ -1,7 +1,7 @@
 export async function POST(request: Request) {
     const body = await request.json();
     console.log(body)
-    return new Response(JSON.stringify({ ok: true, body: body }), {
+    return new Response(JSON.stringify(body), {
         headers: { 'content-type': 'application/json' },
     });
 }
